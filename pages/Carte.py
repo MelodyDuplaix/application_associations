1a40aecafc33fe6e4f6e1206ffe9b19a51f7e686
MelodyDuplaix <131142776+MelodyDuplaix@users.noreply.github.com>
2025-02-11 16:30:22 +0100

    Update Carte.py

--- pages/Carte.py
+++ pages/Carte.py
@@ -3,7 +3,6 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 from bibliotheque.lib import  *
-from st_pages import Page, show_pages, add_page_title
 from datetime import datetime 
 from plotly.offline import iplot
 import plotly.graph_objs as go
@@ -12,16 +11,6 @@ import numpy as np
 import folium
 import streamlit as st
 from streamlit_folium import st_folium
-
-# config de pages
-st.set_page_config(
-    page_title="Localisation des associations",
-    page_icon="📊",
-    menu_items={
-        "Get Help": "https://www.cefim.eu/",
-        "About" : "https://www.linkedin.com/in/melody-duplaix-391672265"
-    }
-)
 
 st.title("Localisation des associations")
 
